bc64059e06ca1436c83fe7de475238836061e519
Luiz Henrique <luizgthub@gmail.com>
2024-06-14 17:36:58 -0300

    pra quem eu amo

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "min1.jpeg",
+    min2: "min2.jpeg",
+    min3: "max3.jpeg",
+    min4: "max4.jpeg",
+    min5: "min5.jpeg",
+    min6: "min6.jpeg",
+
+    max1: "max1.jpeg",
+    max2: "max2.jpeg",
+    max3: "max3.jpeg",
+    max4: "max4.jpeg",
+    max5: "max5.jpeg",
+    max6: "max6.jpeg",
+}
